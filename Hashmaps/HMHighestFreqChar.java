093ce036157e2cb00ff65b03d74f3edfa87b2748
Jai Kumar <54082842+jaikumxr@users.noreply.github.com>
2021-01-22 18:44:54 +0530

    BST Merge (#1)
    * Binary Search Tree Final
    
    * Hashmaps 1

---
+++ Hashmaps/HMHighestFreqChar.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class HMHighestFreqChar {
+
+    public static void hello() {
+        System.out.println("Jai");
+    }
+
+    public static void main(String[] args) throws Exception {
+        hello();
+    }
+
+}
