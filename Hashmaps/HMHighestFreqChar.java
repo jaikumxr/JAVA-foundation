469e5705d1acd8516e7cfab594f2f7afd5656ffa
Jai Kumar <jaikumxr@gmail.com>
2021-01-28 19:28:02 +0530

    Hashmaps Final

--- Hashmaps/HMHighestFreqChar.java
+++ Hashmaps/HMHighestFreqChar.java
@@ -1,14 +1,37 @@
 import java.io.*;
 import java.util.*;
 
+//Program to print the character with the highest frequency in a String, using Hashmaps
+
 public class HMHighestFreqChar {
 
-    public static void hello() {
-        System.out.println("Jai");
+//take one, accepted
+
+    public static Character hfc(String str) {
+        HashMap<Character, Integer> h1 = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            Character ch = str.charAt(i);
+            if(h1.containsKey(ch)){
+                h1.put(ch, h1.get(ch)+1);
+            } else {
+                h1.put(ch, 1);
+            }
+        }
+        Character maxc = null;
+        int max  = Integer.MIN_VALUE;
+        for (Character c : h1.keySet()) {
+            if(h1.get(c)>max){
+                max = h1.get(c);
+                maxc = c;
+            }
+        }
+        return maxc;
     }
 
     public static void main(String[] args) throws Exception {
-        hello();
+        Scanner scn = new Scanner(System.in);
+        String s = scn.nextLine();
+        System.out.println(hfc(s));
     }
 
 }
