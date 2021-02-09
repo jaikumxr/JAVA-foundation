30c958f7efd4c9ed0083cf23daa6581d5ce230c2
Jai Kumar <jaikumxr@gmail.com>
2021-02-09 14:20:12 +0530

    Graphs 1

---
+++ Graphs/GraphConstructor.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class GraphConstructor {
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+    }
+
+}
